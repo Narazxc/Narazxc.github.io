ba2c9215173cc622bfea03287377aa2c68e1bac7
Sitha Sovannara <sithasovannara51@gmail.com>
2024-01-24 19:29:47 +0700

    Update projectData object

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ project }) {
   return (
-    <a href={project.link} target="_blank" rel="noreferrer">
+    <a href={project.url} target="_blank" rel="noreferrer">
       <div className="h-52 rounded-md bg-purple-300 hover:bg-green-300">
         {project.name}
       </div>
